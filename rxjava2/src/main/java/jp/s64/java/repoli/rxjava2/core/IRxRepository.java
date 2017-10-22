d16e6c37b32f88985fa51362eb5cb3beb0a4d1c6
Shuma Yoshioka <s64.stdio@gmail.com>
2017-10-22 20:05:09 +0900

    Create core interfaces

---
+++ rxjava2/src/main/java/jp/s64/java/repoli/rxjava2/core/IRxRepository.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017 Shuma Yoshioka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.s64.java.repoli.rxjava2.core;
+
+import org.jetbrains.annotations.NotNull;
+
+import io.reactivex.Single;
+import jp.s64.java.repoli.core.IDataKey;
+import jp.s64.java.repoli.core.IExpirePolicy;
+import jp.s64.java.repoli.core.IRemovePolicy;
+import jp.s64.java.repoli.core.IRepositoryDataContainer;
+
+public interface IRxRepository<TB, AB> {
+
+    @NotNull
+    <T extends TB, A extends AB> Single<IRepositoryDataContainer<T, A>> get(@NotNull IDataKey<T, A> key, @NotNull IRxStorage<TB, AB> storage, @NotNull IExpirePolicy<TB, AB> policy, @NotNull IRxProvider<TB, AB> provider);
+
+    @NotNull
+    <T extends TB, A extends AB> Single<Integer> remove(@NotNull IDataKey<T, A> key, @NotNull IRxStorage<TB, AB> storage, @NotNull IRemovePolicy<TB, AB> policy);
+
+}
---
+++ rxjava2/src/main/java/jp/s64/java/repoli/rxjava2/core/IRxStorage.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2017 Shuma Yoshioka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.s64.java.repoli.rxjava2.core;
+
+import org.jetbrains.annotations.NotNull;
+
+import io.reactivex.Single;
+import jp.s64.java.repoli.core.IDataKey;
+import jp.s64.java.repoli.core.IRepositoryDataContainer;
+
+public interface IRxStorage<TB, AB> {
+
+    @NotNull
+    <T extends TB, A extends AB> Single<IRepositoryDataContainer<T, A>> getAsync(@NotNull IDataKey<T, A> key);
+
+    @NotNull
+    Single<Integer> removeAsync(@NotNull IDataKey<?, ?> key);
+
+    @NotNull
+    Single<Integer> removeRelativesAsync(@NotNull IDataKey<?, ?> key);
+
+    @NotNull
+    <T extends TB, A extends AB> Single<IRepositoryDataContainer<T, A>> saveAsync(@NotNull IDataKey<T, A> key, @NotNull IRepositoryDataContainer<T, A> container);
+
+}
